749a733965cea9c54fd8d5e49f910cd06e6816a0
Acer <goskovd2017@mail.ru>
2024-04-19 22:45:43 +0300

    Added database

---
+++ Models/Superpower.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestAPI.Models;
+
+public partial class Superpower
+{
+    public int Id { get; set; }
+
+    public string? PowerName { get; set; }
+}
